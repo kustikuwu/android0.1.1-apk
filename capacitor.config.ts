0ce716453d2c814768bda389eeb3185403a75236
kustik uwu <arkakuznak@gmail.com>
2025-06-04 03:17:09 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.roomrescue.game',
+  appName: 'Room Rescue',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
